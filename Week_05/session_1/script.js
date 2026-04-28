b496034b231ba677ac9f4de9fa2b1b7d8f885628
Than-sothearak <thearak643@gmail.com>
2026-04-28 10:35:45 +0700

    add:week-5 code

--- Week_05/session_1/script.js
+++ Week_05/session_1/script.js
@@ -1,12 +1,37 @@
-let num1 = 10;
-let num2 = 5;
+// Hello Console
+console.log("Hello, World! I am learning JavaScript.");
 
-let score = 94;
+// Variables and Data Types
 
-for (let i = 1; i <= 20; i++) {
-  if (i % 2 === 0) {
-    console.log(i);
-  } else {
-    console.log(i + " is odd");
-  }
-}
+// String
+const myName = "Dara"; 
+// Number
+const myAge = 30;
+// Boolean
+const isStudent = true;
+
+console.log("My name is " + myName);
+console.log("I am " + myAge + " years old");
+console.log("Am I a student? " + isStudent);
+
+// Reading Errors
+// let myCity = "New York";
+// myCity = "Los Angeles"; // Reassigning the value of myCity
+
+// const myCountry = "USA";
+// myCountry = "Canada"; // This will cause an error because myCountry is a constant
+
+
+
+// let num1 = 10;
+// let num2 = 5;
+
+// let score = 94;
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   } else {
+//     console.log(i + " is odd");
+//   }
+// }
