129128440c43f257c6fe5ce2d396f9236d8ff4c5
Thearak <thearak643@gmail.com>
2026-04-30 13:10:54 +0700

    fix:week-5

---
+++ Week_05/script.js
@@ -0,0 +1,12 @@
+const studentName = "Dara";
+let studentAge = 20;
+let isEnrolled = true
+
+
+
+let greeting = `Hello, my name is ${studentName} and I am ${studentAge} years old.` 
+
+console.log(greeting);
+
+// Output: Hello, my name is Sok and I am 20 years old.
+
--- Week_05/session_1/script.js
+++ Week_05/session_1/script.js
@@ -1,43 +1,106 @@
-// 1: Hello Console
-console.log("Hello, World! I am learning JavaScript.");
+const myName = "Thearak" //String
+
+let myAge = 29; //Number
+
+let isStudent = false; //Boolean
+
+// console.log(myName)
+// console.log(myAge)
+// console.log(isStudent)
+
+
+let num1 = 10
+let num2 = 5 
+
+console.log("Addition:", num1 + num2);       // 15
+console.log("Subtraction:", num1 - num2);    // 5
+console.log("Multiplication:", num1 * num2); // 50
+console.log("Division:", num1 / num2);       // 2
+
+
+let studentName = "Dara"
+let studentAge = 20;
+
+let greeting = "Hello, my name is" + studentName +
+               " and I am " + studentAge + " years old.";
+
+let greeting2 = `Hello, my name is ${studentName} and I am ${studentAge} year old.`
+
+
+console.log(greeting2);
 
 
 
 
 
 
-// 2: Variables and Data Types 45 Minutes
-
-// String
-const myName = "Dara"; 
-// Number
-const myAge = 30;
-// Boolean
-const isStudent = true;
-
-console.log("My name is " + myName);
-console.log("I am " + myAge + " years old");
-console.log("Am I a student? " + isStudent);
-
-// Reading Errors
-let myCity = "New York";
-myCity = "Los Angeles"; // Reassigning the value of myCity
-
-const myCountry = "USA";
-myCountry = "Canada"; // This will cause an error because myCountry is a constant
 
 
-// 3: Operators and String Concatenation 30 Minutes
 
-let num1 = 10;
-let num2 = 5;
 
-let score = 94;
 
-for (let i = 1; i <= 20; i++) {
-  if (i % 2 === 0) {
-    console.log(i);
-  } else {
-    console.log(i + " is odd");
-  }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // 1: Hello Console
+// console.log("Hello, World! I am learning JavaScript.");
+
+
+
+
+
+
+// // 2: Variables and Data Types 45 Minutes
+
+// // String
+// const myName = "Dara"; 
+// // Number
+// const myAge = 30;
+// // Boolean
+// const isStudent = true;
+
+// console.log("My name is " + myName);
+// console.log("I am " + myAge + " years old");
+// console.log("Am I a student? " + isStudent);
+
+// // Reading Errors
+// let myCity = "New York";
+// myCity = "Los Angeles"; // Reassigning the value of myCity
+
+// const myCountry = "USA";
+// myCountry = "Canada"; // This will cause an error because myCountry is a constant
+
+
+// // 3: Operators and String Concatenation 30 Minutes
+
+// let num1 = 10;
+// let num2 = 5;
+
+// let score = 94;
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   } else {
+//     console.log(i + " is odd");
+//   }
+// }
